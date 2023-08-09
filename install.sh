b44e26d4989c873aec2606cefc223300d4222513
Junhui Li <junhui.lee@icloud.com>
2023-08-09 14:59:17 +0800

    update zsh prompt

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 SHELL_FOLDER=$(cd `dirname $0`; pwd)
 # zsh
-
 if [ -e $HOME/.zshrc ]
 then
-    echo ".zshrc already exist"
+    mv $HOME/.zshrc $HOME/.zshrc_bak
+    echo "your .zshrc will be moved to ~/.zshrc_bak"
+    ln -s $SHELL_FOLDER/zsh/.zshrc $HOME/.zshrc
 else
-    #mv $HOME/.zshrc $HOME/.zshrc_bak
     ln -s $SHELL_FOLDER/zsh/.zshrc $HOME/.zshrc
 fi
